a490a28239c675a186807419eeb6a25398d502dc
AMVMesias <mesiasv2@gmail.com>
2025-05-22 11:19:35 -0500

    Agregar archivos de proyecto.

---
+++ Taller Estrella/FrmStar1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Taller_Estrella
+{
+    partial class FrmStar1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTurnRigth = new System.Windows.Forms.Button();
+            this.btnTurnLeft = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbCanvas = new System.Windows.Forms.GroupBox();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.grbCanvas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTurnRigth
+            // 
+            this.btnTurnRigth.Location = new System.Drawing.Point(126, 90);
+            this.btnTurnRigth.Name = "btnTurnRigth";
+            this.btnTurnRigth.Size = new System.Drawing.Size(75, 23);
+            this.btnTurnRigth.TabIndex = 24;
+            this.btnTurnRigth.Text = "Derecha 5º";
+            this.btnTurnRigth.UseVisualStyleBackColor = true;
+            // 
+            // btnTurnLeft
+            // 
+            this.btnTurnLeft.Location = new System.Drawing.Point(34, 90);
+            this.btnTurnLeft.Name = "btnTurnLeft";
+            this.btnTurnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnTurnLeft.TabIndex = 23;
+            this.btnTurnLeft.Text = "izquierda 5º";
+            this.btnTurnLeft.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Girar";
+            // 
+            // grbCanvas
+            // 
+            this.grbCanvas.Controls.Add(this.picCanvas);
+            this.grbCanvas.Font = new System.Drawing.Font("Sans Serif Collection", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbCanvas.Location = new System.Drawing.Point(279, 21);
+            this.grbCanvas.Name = "grbCanvas";
+            this.grbCanvas.Size = new System.Drawing.Size(487, 409);
+            this.grbCanvas.TabIndex = 21;
+            this.grbCanvas.TabStop = false;
+            this.grbCanvas.Text = "Gráfico";
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Location = new System.Drawing.Point(0, 19);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(481, 384);
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            // 
+            // FrmStar1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTurnRigth);
+            this.Controls.Add(this.btnTurnLeft);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grbCanvas);
+            this.Name = "FrmStar1";
+            this.Text = "FrmStar";
+            this.grbCanvas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTurnRigth;
+        private System.Windows.Forms.Button btnTurnLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbCanvas;
+        private System.Windows.Forms.PictureBox picCanvas;
+    }
+}
